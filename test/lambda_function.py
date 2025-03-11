4f8c8a87be078ebef38b9023c4ca09586add3b8f
Deblyn Prado <deblynprado@hotmail.com>
2025-03-11 15:24:53 -0300

    Creating lambda functions

--- test/lambda_function.py
+++ test/lambda_function.py
@@ -15,8 +15,8 @@ postMehod = 'POST'
 patchMethod = 'PATCH'
 deleteMethod = 'DELETE'
 healthPath = '/health'
-product = '/product'
-products = 'products'
+carPath = '/car'
+carsPath = '/cars'
 
 def lambda_handler(event, context):
   logger.info(event)
@@ -24,6 +24,101 @@ def lambda_handler(event, context):
   path = event['path']
   if httpMethod == getMethod and path == healthPath : 
     response = buildresponse(200)
+  elif httpMethod == getMethod and path == carPath :
+    response = getCar(event['queryStringParameters']['carId'])
+  elif httpMethod == getMethod and path == carsPath :
+    response = getCars()
+  elif httpMethod == postMehod and path == carPath :
+    response = saveCar(json.loads(event['body']))
+  elif httpMethod == patchMethod and path == carPath :
+    requestBody = json.loads(event['body'])
+    response = modifyCar(requestBody['carId'], requestBody['updateKey'], requestBody['updateValue'])
+  elif httpMethod == deleteMethod and path == carPath :
+    requestBody == json.loads(event['body'])
+    response = deleteCar(requestBody['carId'])
+  else :
+    response = buildresponse(404, {'message': 'Not Found'})
+    
+  return response
+
+def getCar(carId) :
+  try :
+    response = table.get_item(
+      Key={
+        'carId': carId
+      }
+    )
+    if 'Item' in response :
+      return buildresponse(200, response['Item'])
+    else :
+      return buildresponse(404, {'message': 'Car not found' % carId})
+  except :
+    logger.exception('Do your custom error handling here. I am just gonna log it out here!!')
+    
+def getCars() : 
+  try :
+    response = table.scan()
+    result = response['Item']
+    
+    while 'lastEvaluatedKey' in response :
+      response = table.sacan(ExclusiveStartKey=response['lastEvaluatedKey'])
+      result.extend(response['Item'])
+      
+    body = {
+      'cars': response
+    }
+    return buildresponse(200, body)
+  except :
+    logger.exception('Do your custom error handling here. I am just gonna log it out here!!')
+    
+def saveCar(requestBody) :
+  try :
+    table.put_item(Item=requestBody)
+    body = {
+      'Operation': 'SAVE',
+      'Message': 'SUCCESS' ,
+      'Item': requestBody
+    }
+  except :
+    logger.exception('Do your custom error handling here. I am just gonna log it out here!!')
+    
+def modifyCar(carId, updateKey, updateValue) :
+  try :
+    response = table.update_item(
+      Key={
+        'carId': carId
+      },
+      UpdateExpression='set %s = :val' % updateKey,
+      ExpressionAttributeValues={
+        ':value': updateValue
+      },
+      ReturnValues='UPDATED_NEW'
+    )
+    body = {
+      'Operation': 'UPDATE',
+      'Message': 'SUCCESS',
+      'UpdatedAttributes': response
+    }
+    return buildresponse(200, body)
+  except :  
+    logger.exception('Do your custom error handling here. I am just gonna log it out here!!')
+    
+def deleteCar(carId) :
+  try :
+    response = table.delete_item(
+      Key={
+        'carId': carId
+      },
+      ReturnValues='ALL_OLD'
+    )
+    body = {
+      'Operation': 'DELETE',
+      'Message': 'SUCCESS',
+      'Item': response
+    }
+    return buildresponse(200, body)
+  except :
+    logger.exception('Do your custom error handling here. I am just gonna log it out here!!')
     
 def buildresponse(statusCode, body=None) :
   response = {
@@ -34,5 +129,5 @@ def buildresponse(statusCode, body=None) :
     }
   }
   if body is not None :
-    response['body'] = json.dumps(body)
+    response['body'] = json.dumps(body, cls=CustomEncoder)
   return response
